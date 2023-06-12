ae7df5f039aad92442dbaded48013bda5b9f0ace
Adam Sitarcik <adamsitarcik21@gmail.com>
2023-06-12 20:05:03 +0200

    change displaying of errors in the weathercard

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import '@fontsource/roboto';
 import './popup.css';
 import WeatherCard from './WeatherCard/WeatherCard';
 
